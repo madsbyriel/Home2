cfa9028d1cf6d27bdc4ca35f9ce74bcb1b49aa38
Mads Byriel <madshbyriel@gmail.com>
2021-10-06 00:49:29 +0200

    Compaibility with madsbyriel.pythonanywhere.com, ready to clone

--- Home2/settings.py
+++ Home2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dts)##t)(7$xx@rjrs7v5)k^13l*ww7jj-o4()+x9w#8i79s0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['madsbyriel.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Home2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
